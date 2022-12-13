179d1e88df1d5e45bd50b66201ba3d82c51404a6
hbclone <a81316575@gmail.com>
2022-12-13 17:42:21 +0900

    프로젝트 생성 MSW연결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import initMockAPI from "./mocks";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  initMockAPI();
+
+  return <Component {...pageProps} />;
 }
