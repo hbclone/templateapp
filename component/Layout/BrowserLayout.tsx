ee5b7d837e7f9520700fc3eb288fe10f2ac18d19
hbclone <a81316575@gmail.com>
2023-01-10 19:11:53 +0900

    사이드바 수정

--- component/Layout/BrowserLayout.tsx
+++ component/Layout/BrowserLayout.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 import React, { ReactElement } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Cube from "styles/Asset/cube.png";
-import { Column } from "styles/theme";
+import { Column, Row } from "styles/theme";
 import { ReducerType } from "pages/store/modules/RootReducer";
 import { changeMode, ColorMode } from "pages/store/modules/ColorMode";
 import styled from "styled-components";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 interface Menu {
   children: ReactElement;
 }
@@ -22,19 +25,43 @@ const BrowserLayout = ({ children }: Menu) => {
     <Layout>
       <Menu>
         <ImageWrapper>
-          <Image
-            onClick={changeItem}
-            src={Cube}
-            alt="mainImage"
-            width={40}
-            height={40}
-          />
+          <Image src={Cube} alt="mainImage" width={40} height={40} />
+
+          <Row style={{ gap: "1rem", alignItems: "center" }}>
+            <NotificationsNoneOutlinedIcon
+              style={{
+                width: 25,
+                height: 25,
+                cursor: "pointer",
+                color: "#262626",
+              }}
+            />
+
+            {Color.mode ? (
+              <DarkModeIcon
+                onClick={changeItem}
+                style={{
+                  width: 20,
+                  height: 20,
+                  cursor: "pointer",
+                  color: "#262626",
+                }}
+              />
+            ) : (
+              <LightModeIcon
+                onClick={changeItem}
+                style={{
+                  width: 20,
+                  height: 20,
+                  cursor: "pointer",
+                  color: "#262626",
+                }}
+              />
+            )}
+          </Row>
         </ImageWrapper>
       </Menu>
-      <Column>
-        {children}
-        <H1 color={"red"}>{Color.mode ? "dark" : "white"}</H1>
-      </Column>
+      <Column>{children}</Column>
     </Layout>
   );
 };
@@ -51,20 +78,33 @@ const Layout = styled.div`
 
 const Menu = styled.div`
   display: flex;
-  width: 10rem;
+  flex-direction: column;
+  width: 30rem;
   height: 100%;
   background: ${({ theme }) => theme.text.primary};
+  align-items: center;
+  justify-content: space-between;
   border-right: ${({ theme }) => `1px solid ${theme.border.primary}`};
+  border-top-right-radius: 1rem;
+  border-bottom-right-radius: 1rem;
   background: ${({ theme }) => theme.bg.side};
   transition: all ease 1s 0s;
 `;
 
 const ImageWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
+  padding: 1rem;
+  width: 100%;
+  height: 5rem;
+  cursor: pointer;
+`;
+
+const ModeWrapper = styled.div`
+  display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 5rem;
-  cursor: pointer;
+  margin-bottom: 5rem;
 `;
